4cf570798f8243a4ce72eb2947333ccfaebe5ef6
gabrielLoboo <gabrielfrancisolobo@gmail.com>
2023-06-07 00:41:54 -0300

    scrollButton feito

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("scrollButton").addEventListener("click", function(){
+    var targetSection = document.getElementById("section-ag");
+    targetSection.scrollIntoView({ behavior: "smooth" });
+});
