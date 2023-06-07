b2f11783e0e629810f5ad4d85531d187da7ad5cf
kayqueln <rm550782@fiap.com.br>
2023-06-07 17:59:59 -0300

    contato pronta (função clique em andamento)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 
-document.getElementById("scrollButton").addEventListener("click", function(){
-    var targetSection = document.getElementById("section-ag");
-    targetSection.scrollIntoView({ behavior: "smooth" });
+// document.getElementById("scrollButton").addEventListener("click", function(){
+//     var targetSection = document.getElementById("section-ag");
+//     targetSection.scrollIntoView({ behavior: "smooth" });
+// });
+
+const enviar = document.querySelector("button")
+const nome = document.getElementById("nome")
+
+enviar.addEventListener("click", function(){
+    if (nome.value === ''){
+        alert('Preencha os campos')
+    }
 });
