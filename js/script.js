a458ea54512b278ddc30ab10d8cfb3c43c6b0380
kayqueln <rm550782@fiap.com.br>
2023-06-07 21:24:20 -0300

    correção ná página "ia generativa"

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
-// document.getElementById("scrollButton").addEventListener("click", function(){
-//     var targetSection = document.getElementById("section-ag");
-//     targetSection.scrollIntoView({ behavior: "smooth" });
-// });
+document.getElementById("scrollButton").addEventListener("click", function(){
+    var targetSection = document.getElementById("section-ag");
+    targetSection.scrollIntoView({ behavior: "smooth" });
+});
 
 const enviar = document.querySelector("button")
 const assunto = document.getElementById("assunto")
