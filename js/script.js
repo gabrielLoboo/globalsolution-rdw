ed2673d4da105f473f3c28961f3c14bfebad7a49
kayqueln <rm550782@fiap.com.br>
2023-06-07 21:30:24 -0300

    correção nas funções js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
-
 document.getElementById("scrollButton").addEventListener("click", function(){
     var targetSection = document.getElementById("section-ag");
     targetSection.scrollIntoView({ behavior: "smooth" });
 });
 
-const enviar = document.querySelector("button")
+
 const assunto = document.getElementById("assunto")
 const email = document.getElementById("email")
 const mensagem = document.getElementById("mensagem")
 
-enviar.addEventListener("click", function(){
+document.querySelector("button").addEventListener("click", function(){
     if (validaCampos()){
         alert('Por favor, preencha os campos pedidos!')
     } else{
